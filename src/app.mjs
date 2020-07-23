d236154ad5925e785bbb2945810106ce574ff932
Stevan-Radovanovic <stevan.radovanovic@mmklab.org>
2020-07-23 15:40:28 +0200

    Updated RM, added Data to db, tested code

--- src/app.mjs
+++ src/app.mjs
@@ -3,8 +3,24 @@ import DataBase from "./database/database.mjs";
 
 const app = Express();
 
-DataBase.execute("SELECT * FROM players").then((players) => {
+DataBase.execute("SELECT * FROM player").then((players) => {
   console.log(players[0]);
+});
+
+DataBase.execute("SELECT * FROM contract").then((contracts) => {
+  console.log(contracts[0]);
+});
+
+DataBase.execute("SELECT * FROM game").then((games) => {
+  console.log(games[0]);
+});
+
+DataBase.execute("SELECT * FROM contractbonus").then((bonuses) => {
+  console.log(bonuses[0]);
+});
+
+DataBase.execute("SELECT * FROM stats").then((stats) => {
+  console.log(stats[0]);
 });
 
 app.use((err, req, res, next) => {
