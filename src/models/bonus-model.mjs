b464d1e8f4f1ae0bb4a1c2c9c13c9677149c0656
Stevan-Radovanovic <stevan.radovanovic@mmklab.org>
2020-07-29 17:19:48 +0200

    Changed table name

--- src/models/bonus-model.mjs
+++ src/models/bonus-model.mjs
@@ -1,21 +1,27 @@
 import Sequelize from "sequelize";
 import sequelize from "../database/database.mjs";
 
-const Bonus = sequelize.define("bonus", {
-  id: {
-    type: Sequelize.INTEGER,
-    allowNull: false,
-    autoIncrement: true,
-    primaryKey: true,
+const Bonus = sequelize.define(
+  "bonuses",
+  {
+    id: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    description: {
+      type: Sequelize.TEXT,
+      allowNull: false,
+    },
+    amount: {
+      type: Sequelize.REAL,
+      allowNull: false,
+    },
   },
-  description: {
-    type: Sequelize.TEXT,
-    allowNull: false,
-  },
-  amount: {
-    type: Sequelize.REAL,
-    allowNull: false,
-  },
-});
+  {
+    freezeTableName: true,
+  }
+);
 
 export default Bonus;
