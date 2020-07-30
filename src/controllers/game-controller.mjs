338f9fae97aa21e4c1a94c6dee99d2abcee7d3d1
Stevan-Radovanovic <stevan.radovanovic@mmklab.org>
2020-07-30 12:04:47 +0200

    Added game routes and controller

--- src/controllers/game-controller.mjs
+++ src/controllers/game-controller.mjs
@@ -1,1 +1,47 @@
 import Game from "../models/game-model.mjs";
+
+const getGames = async (req, res, next) => {
+  const result = await Game.findAll();
+  res.json({ games: result });
+};
+
+const getGameById = async (req, res, next) => {
+  const id = req.params.id;
+  const result = await Game.findByPk(id);
+  res.json({ game: result });
+};
+
+const postGame = async (req, res, next) => {
+  const result = await Game.create({
+    opponentName: req.body.opponentName,
+    result: req.body.result,
+    dateOfPlaying: req.body.dateOfPlaying,
+  });
+  res.json({ message: "New game created", game: result });
+};
+
+const updateGame = async (req, res, next) => {
+  const result = await Game.update(
+    {
+      opponentName: req.body.opponentName,
+      result: req.body.result,
+      dateOfPlaying: req.body.dateOfPlaying,
+    },
+    { where: { id: req.params.id } }
+  );
+  res.json({
+    message: result[0] === 0 ? "Game not updated" : "Game updated",
+    result: result,
+  });
+};
+
+const deleteGame = async (req, res, next) => {
+  const id = req.params.id;
+  const result = await Game.destroy({ where: { id: id } });
+  res.json({
+    message: result === 0 ? "Game not deleted" : "Game deleted",
+    result: result,
+  });
+};
+
+export { getGames, getGameById, postGame, updateGame, deleteGame };
--- src/routes/game-routes.mjs
+++ src/routes/game-routes.mjs
@@ -2,4 +2,10 @@ import express from "express";
 import * as controller from "../controllers/game-controller.mjs";
 const router = express.Router();
 
+router.get("/", controller.getGames);
+router.get("/:id", controller.getGameById);
+router.post("/", controller.postGame);
+router.put("/:id", controller.updateGame);
+router.delete("/:id", controller.deleteGame);
+
 export default router;
