3b67d3ed8ff353754c2c03fd983c92248c43377d
Stevan-Radovanovic <stevan.radovanovic@mmklab.org>
2020-07-30 21:14:08 +0200

    Added Allow middleware

--- src/app.mjs
+++ src/app.mjs
@@ -20,6 +20,19 @@ const app = Express();
 
 app.use(Body.json());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, PUT, OPTIONS"
+  );
+  next();
+});
+
 app.use("/players", playerRoutes);
 app.use("/contracts", contractRoutes);
 app.use("/bonuses", bonusRoutes);
