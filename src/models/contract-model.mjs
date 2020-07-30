d9987335fca5dac19aa6bbb720e7372c0d896180
Stevan-Radovanovic <stevan.radovanovic@mmklab.org>
2020-07-30 14:57:50 +0200

    Added validators for the contract model

--- src/models/contract-model.mjs
+++ src/models/contract-model.mjs
@@ -1,25 +1,54 @@
 import Sequelize from "sequelize";
 import sequelize from "../database/database.mjs";
 
-const Contract = sequelize.define("contract", {
-  id: {
-    type: Sequelize.INTEGER,
-    allowNull: false,
-    autoIncrement: true,
-    primaryKey: true,
-  },
-  weeklySalary: {
-    type: Sequelize.REAL,
-    allowNull: false,
-  },
-  startingDate: {
-    type: Sequelize.DATE,
-    allowNull: false,
-  },
-  endingDate: {
-    type: Sequelize.DATE,
-    allowNull: false,
-  },
-});
+const Contract = sequelize.define(
+  "contract",
+  {
+    id: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    weeklySalary: {
+      type: Sequelize.REAL,
+      allowNull: false,
+      validate: {
+        isDecimal: {
+          msg: "IsDecimal validation not passed for WeeklySalary",
+        },
+      },
+    },
+    startingDate: {
+      type: Sequelize.DATE,
+      allowNull: false,
+      validate: {
+        isDate: {
+          msg: "IsDate validation not passed for startingDate",
+        },
+      },
+    },
+    endingDate: {
+      type: Sequelize.DATE,
+      allowNull: false,
+      validate: {
+        isDate: {
+          msg: "IsDate validation not passed for endingDate",
+        },
+      },
+    },
+  }
+  /*
+  {
+    validate: {
+      startDateAfterEndDate() {
+        if (this.startingDate.isAfter(this.endingDate)) {
+          throw new Error("Start date must be before the end date.");
+        }
+      },
+    },
+  }
+  */
+);
 
 export default Contract;
