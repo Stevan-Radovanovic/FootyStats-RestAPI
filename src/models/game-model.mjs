e6c5ade20e8933f5f556074a95534227dccd663c
Stevan-Radovanovic <stevan.radovanovic@mmklab.org>
2020-07-30 17:46:08 +0200

    Added validators for the game model

--- src/models/game-model.mjs
+++ src/models/game-model.mjs
@@ -11,14 +11,30 @@ const Game = sequelize.define("game", {
   result: {
     type: Sequelize.STRING,
     allowNull: false,
+    validate: {
+      is: {
+        args: /(([0-9])+-([0-9])+){1}/i,
+        msg: "Regex validation not passed for Result",
+      },
+    },
   },
   opponentName: {
     type: Sequelize.STRING,
     allowNull: false,
+    validate: {
+      notEmpty: {
+        msg: "NotEmpty validation not passed for OpponentName",
+      },
+    },
   },
   dateOfPlaying: {
     type: Sequelize.DATE,
     allowNull: false,
+    validate: {
+      isDate: {
+        msg: "isDate validation not passed for DateOfPlaying",
+      },
+    },
   },
 });
 
